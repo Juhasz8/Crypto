37c71e5fdcc487e5af66d91ac517cd0cfa6e573a
dvelotto <velottodiego10@gmail.com>
2023-05-14 17:07:58 +0200

    Added Pseudo-Code to CurrentEvent.java

--- POOF UI/src/main/java/com/example/poof_ui/CurrentEvent.java
+++ POOF UI/src/main/java/com/example/poof_ui/CurrentEvent.java
@@ -9,16 +9,21 @@ public class CurrentEvent extends AnchorPane {
     private String eventName;
     private double probability;
 
+    // Constructor to create a CurrentEvent object
     public CurrentEvent(String eventName, double probability) {
         this.eventName = eventName;
         this.probability = probability;
 
+        // Create a new FXMLLoader for loading the FXML file and setting the controller
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXML/CurrentEvent.fxml"));
         fxmlLoader.setController(this);
 
         try {
+            // Load the FXML file and retrieve the root AnchorPane
             fxmlLoader.load();
             AnchorPane root = fxmlLoader.getRoot();
+
+            // Add the root pane to the children of the CurrentEvent object
             this.getChildren().add(root);
         } catch (Exception e) {
             e.printStackTrace();
@@ -28,10 +33,13 @@ public class CurrentEvent extends AnchorPane {
     @FXML
     private Label currentEventName;
 
+    // Method called when the FXML is initialized
     public void initialize() {
+        // Set the text of the currentEventName label to the eventName
         currentEventName.setText(eventName);
     }
 
+    // Getter method for probability
     public double getProbability() {
         return probability;
     }
