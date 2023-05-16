8c156779889d2b96fdd0b2a04b5b836c3f843ce0
arturomegias <arturomegiasc@gmail.com>
2023-05-16 17:05:38 +0200

    saaaaaaaaaaaaaAAAAAAAA

--- POOF UI/src/main/java/com/example/poof_ui/Blockchain_Side/Main.java
+++ POOF UI/src/main/java/com/example/poof_ui/Blockchain_Side/Main.java
@@ -1,7 +1,7 @@
 package com.example.poof_ui.Blockchain_Side;//import javax.swing.*;
 
 
-public class Main {
+public class    Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
